1d5a04c981579094377d26caed5b737c7aebfd3e
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2017-08-13 00:14:03 -0400

    Quick fix for sending html

--- src/server/app/database/firebase.js
+++ src/server/app/database/firebase.js
@@ -1,15 +1,33 @@
 var firebase = require('firebase');
+var admin = require('firebase-admin');
 var path = require('path');
 var CONFIG = require(path.resolve('..','..','config','database-config.json'));
 require('firebase/auth');
 require('firebase/database');
 
-//Initalize firebase app
-try {
-	firebase.initializeApp(CONFIG.config);
-} catch(err) {
-	console.log("Could not initialize database.");
-}
+// try{
+// 	firebase.initializeApp({
+// 	  serviceAccount: path.resolve('..','..','config','database-config.json'),	
+// 	  databaseURL: "https://techblog-4cede.firebaseio.com"
+// 	});
+// } catch(err) {
+// 	console.log("Could not initialize database.");
+// 	console.log(err);
+// }
+
+// try {
+// 	firebase.initializeApp({
+//     apiKey: "AIzaSyBzbCzaaiiwYmzN1Gt8P_iNl-qvzIy0Xzk",
+//     authDomain: "techblog-4cede.firebaseapp.com",
+//     databaseURL: "https://techblog-4cede.firebaseio.com",
+//     projectId: "techblog-4cede",
+//     storageBucket: "techblog-4cede.appspot.com",
+//     messagingSenderId: "649580246596"
+// 	});
+// } catch(err) {
+// 		console.log("Could not initialize database.");
+// 		console.log(err);
+// }
 
 /*
 //Sign-in with server user
@@ -20,6 +38,7 @@ firebase.auth().signInWithEmailAndPassword(CONFIG.user.email, CONFIG.user.passwo
   console.log("Could not authenticate " + CONFIG.user.email);
 });
 */
+
 module.exports = {
 	firebase: firebase
 };
--- src/server/app/routes/posts.js
+++ src/server/app/routes/posts.js
@@ -1,34 +1,54 @@
 var path = require('path');
 var async = require('async');
-var cosmicConfig = require('../../../config/cosmic-config.json')
+var cosmicConfig = require('../../../config/cosmic-config.json');
 
-module.exports = function(firebase, cosmic){
+
+firebase=require('firebase');
+require('firebase/auth');
+require('firebase/database');
+
+
+var CONFIG = require(path.resolve('..','..','config','database-config.json'));
+try {
+	firebase.initializeApp(CONFIG.config);
+} catch(err) {
+		console.log("Could not initialize database.");
+		console.log(err);
+}
+
+
+module.exports = function(f, cosmic){
 	var express = require('express');
 	var router = express.Router();
 
 	router.get('/posts/:slug', function(req, res, next) {
-		console.log(req.params.slug);
-		
 		async.waterfall([
+			//Get slug from firebase
 			function querySlug(callback){
 				var slugValue = null;
-				firebase.database().ref('/posts' + req.params.slug).once('value').then(function(snapshot){
-					slugValue = snapshot.val().slug;
-				});
-				callback(null, slugValue);
-			}, function getCosmicBucket(slug, callback){
+					firebase.database().ref('/posts/' + req.params.slug).once('value').then(function(snapshot){
+						slugValue = snapshot.val().slug;
+						callback(null, slugValue);
+					});		
+			}, 
+			//Get Content from Cosmic
+			function getCosmicBucket(slug, callback){
 				var params = {
-					slug: slug
-				}
-				cosmic.getObject(cosmicConfig, params, function(err, res){
-					res.send(res.content);
+					slug: 'my-first-blog'
+				};
+
+				var config = {};
+				config.bucket = cosmicConfig;
+				cosmic.getObject(config, params, function(err, res){
+					callback(null, res);
 				});
 			}], 
-			function(err){
+			function(err, post){
 				if(err){
 					console.log("REKT");
 				}
-				console.log(req.params.slug);
+				console.log(post);
+				res.send(post.content);
 				res.end();
 		});
 	});
